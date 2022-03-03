4399886266843ff0f6e0463cc13c806c009f979b
Blake Williams <code@shabbyrobe.org>
2022-03-04 10:54:35 +1100

    Remove subsitutions as per https://github.com/markedjs/marked/pull/1532/files

--- src/marked.ts
+++ src/marked.ts
@@ -119,8 +119,6 @@ export class Marked {
     src = src
       .replace(/\r\n|\r/g, "\n")
       .replace(/\t/g, "    ")
-      .replace(/\u00a0/g, " ")
-      .replace(/\u2424/g, "\n")
       .replace(/^ +$/gm, "");
 
     return BlockLexer.lex(src, options, true);
